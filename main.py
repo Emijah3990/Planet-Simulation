5c70d5b4df8c855bd297a411ef767943864b0830
Emijah3990 <emijah_white@proton.me>
2022-10-18 00:32:20 +0200

    v.1

---
+++ main.py
@@ -0,0 +1,118 @@
+import pygame
+
+import math
+
+pygame.init()  # initializing pygame
+
+width, height = 800, 800  # setting up parameters for window
+WIN = pygame.display.set_mode((width, height))  # making window
+pygame.display.set_caption('Planet Simulation')  # title of window
+
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
+
+
+class Planet:
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 225 / AU  # 1 AU = 100 pixels
+    TIMESTEP = 3600 * 24  # 1 day as 1 frame rate
+
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+    def draw(self, win):  # drawing planets
+        x = self.x * self.SCALE + width / 2  # + width used to make planet in the center of the screen
+        y = self.y * self.SCALE + height / 2
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
+
+    def attraction(self, other):
+        # calculating farce of attr. between another obj. and current obj.
+
+        other_x, other_y = other.x, other.y
+        # distance between another and current obj. == r
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+        if other.sun:
+            self.distance_to_sun = distance
+
+        force = self.G * self.mass * other.mass / distance ** 2  # F
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+        return force_x, force_y
+
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+        self.x += self.x_vel * self.TIMESTEP
+        self.y += self.y_vel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()  # setting up frame rate
+
+    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10 ** 30)
+    sun.sun = True
+
+    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10 ** 24)
+    earth.y_vel = 29.783 * 1000
+
+    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10 ** 23)
+    mars.y_vel = 24.077 * 1000
+
+    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10 ** 23)
+    mercury.y_vel = -47.4 * 1000
+
+    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8785 * 10 ** 24)
+    venus.y_vel = -35.02 * 1000
+
+    planets = [sun, earth, mars, mercury, venus]
+
+    while run:  # creating infinite loop
+
+        clock.tick(60)  # 60 fps
+        WIN.fill((0, 0, 0))
+
+        for event in pygame.event.get():  # getting us all events such as key presses, mouse etc.
+            if event.type == pygame.QUIT:  # if user hits x button on window it'll close
+                run = False
+
+        for planet in planets:
+            planet.update_position(planets)
+            planet.draw(WIN)
+
+        pygame.display.update()  # updating screen
+
+    pygame.quit()
+
+
+main()
